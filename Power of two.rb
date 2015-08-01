07e3eb60e6d4394b6471c2b1984babdade063888
seokseoung <duwlehduwl@naver.com>
2015-08-01 21:05:34 +0900

    Create Power of two.rb

---
+++ Power of two.rb
@@ -0,0 +1,8 @@
+def PowersofTwo(num)
+
+  return num&(num-1)==0
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+PowersofTwo(STDIN.gets)           
